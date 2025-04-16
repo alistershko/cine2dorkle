0331af3d9f7c11fd6e8ecf055c2b06781f8d4dc2
StrawberryScot <benjaminfloveday@hotmail.co.uk>
2025-04-16 14:50:15 +0100

    added random movie functionality to getRandomMovie function

--- api/controllers/game.js
+++ api/controllers/game.js
@@ -11,10 +11,23 @@ const getRandomMovie = async (req, res) => {
         }
     };
 
-    fetch(url, options)
-        .then(res => res.json())
-        .then(json => console.log(json))
-        .catch(err => console.error(err));
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+
+        const popularMovies = data.results;
+        if (!popularMovies || popularMovies.length === 0) {
+            res.status(404).json({ error: 'No movies found' });
+        }
+
+        const randomIndex = Math.floor(Math.random() * popularMovies.length);
+        const randomMovie = popularMovies[randomIndex];
+
+        res.json(randomMovie);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch movie data' });
+    }
 }
 
 const GameController = {
