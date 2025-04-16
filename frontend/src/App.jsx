244dc66f6dfcd41f84925361edd7fb42d0aac3b0
Emily Sadler <emily@sadler.gg>
2025-04-16 11:31:30 +0100

    Quick fix for dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ".App.css";
+import "./App.css";
 import { Game } from "./pages/Game/Game";
 import { HomePage } from "./pages/Home/HomePage";
 
