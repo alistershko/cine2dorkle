38204d1c7e269637ef55b76592f625cee23c362e
Etienne <etilegoater@gmail.com>
2025-04-16 12:02:40 +0100

    Starting on frontend service routes

---
+++ frontend/src/services/movies.js
@@ -0,0 +1,31 @@
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+export const getMovies = async () => {
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    const response = await fetch(`${BACKEND_URL}/movies`, requestOptions);
+    if (response.status !== 200) {
+        throw new Error('Failed to fetch movies');
+    }
+    const data = await response.json();
+    return data;
+};
+
+export const getpopularMovie = async () => {
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    const response = await fetch(`${BACKEND_URL}/movie/popular`, requestOptions);
+    if (response.status !== 200) {
+        throw new Error('Failed to fetch popular movies');
+    }
+    const data = await response.json();
+    return data;
+}
