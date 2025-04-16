316af14ba314dc4e6d7c7c59276e531f8f007404
StrawberryScot <benjaminfloveday@hotmail.co.uk>
2025-04-16 16:56:51 +0100

    added getCastFromMovieId function into game controller

--- api/controllers/game.js
+++ api/controllers/game.js
@@ -37,8 +37,45 @@ const getRandomMovie = async (req, res) => {
     }
 }
 
+const getCastFromMovieId = async (req, res) => {
+    const movieId = req.params.id;
+    const url = `https://api.themoviedb.org/3/movie/${movieId}/credits`;
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        }
+    };
+    try {
+        const movies = [];
+        const response = await fetch(url, options);
+        const data = await response.json();
+        const cast = data.cast;
+
+        if (!cast || cast.length === 0) {
+            res.status(404).json({ error: 'No cast found' });
+        }
+
+        console.log(cast.length);
+        
+    const sanitisedCast = [];
+        for (const member of cast) {
+            console.log(member);
+            const { name, id } = member;
+            sanitisedCast.push({ name, id })
+        }
+
+        res.json(sanitisedCast);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch movie data' });
+    }
+}
+
 const GameController = {
     getRandomMovie: getRandomMovie,
+    getCastFromMovieId: getCastFromMovieId,
 };
 
 module.exports = GameController;
--- api/routes/game.js
+++ api/routes/game.js
@@ -4,5 +4,6 @@ const GameController = require("../controllers/game");
 const router = express.Router();
 
 router.get("/initialMovie", GameController.getRandomMovie);
+router.get("/cast/:id", GameController.getCastFromMovieId)
 
 module.exports = router;
