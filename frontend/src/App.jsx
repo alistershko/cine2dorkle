972d8c26e0d723b641172c3e68362f88c32f71b5
Fliss <fliss_d@hotmail.co.uk>
2025-04-16 13:57:38 +0100

    timer added to Timer.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { Header } from "./components/header.jsx";
+import Timer from "./components/Timer";
 
 function App() {
   const [count, setCount] = useState(0);
