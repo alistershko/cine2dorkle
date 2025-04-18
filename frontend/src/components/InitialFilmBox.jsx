cf7bd059bc14d01c7f26c29af73347477dbdbcae
Emily Sadler <emily@sadler.gg>
2025-04-18 09:49:03 +0100

    Added Image to movie card, changed css to accomodate

--- frontend/src/components/InitialFilmBox.jsx
+++ frontend/src/components/InitialFilmBox.jsx
@@ -10,8 +10,7 @@ const InitialFilmBox = ({ movie }) => {
       if (movie && movie.id) {
         try {
           const fetchedCast = await getCastFromMovieId(movie.id);
-          console.log("Fetched cast:", fetchedCast);
-          setCast(fetchedCast.slice(0, 12)); // Limit to the first 10 actors
+          setCast(fetchedCast.slice(0, 12)); // Limit to the first 12 actors
         } catch (error) {
           console.error("Error fetching movie cast:", error);
         }
@@ -25,17 +24,29 @@ const InitialFilmBox = ({ movie }) => {
     return <div>No movie data available</div>;
   }
 
+  const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  const releaseYear = movie.release_date
+    ? new Date(movie.release_date).getFullYear()
+    : "Unknown";
+
   return (
     <div className="FilmBox-Container">
-      <h2 className="Film-Title">{movie.title}</h2>
-      <p className="Film-Release-Year">Released: {movie.release_date}</p>
-      <h3 className="Film-Cast">Cast:</h3>
-      <div className="Film-Cast-Grid">
-        {cast.map((actor, index) => (
-          <div key={index} className="Film-Cast-Card">
-            <div className="Actor-Name">{actor.name}</div>
-          </div>
-        ))}
+      <img
+        src={posterUrl}
+        alt={`${movie.title} Poster`}
+        className="Film-Poster"
+      />
+      <div className="Film-Details">
+        <h2 className="Film-Title">{movie.title}</h2>
+        <p className="Film-Release-Year">{releaseYear}</p>
+        <h3 className="Film-Cast">Cast:</h3>
+        <div className="Film-Cast-Grid">
+          {cast.map((actor, index) => (
+            <div key={index} className="Film-Cast-Card">
+              <div className="Actor-Name">{actor.name}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
