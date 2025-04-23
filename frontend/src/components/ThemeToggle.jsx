5512f00134a93bceca77ac0cd05411f0c3e40943
StrawberryScot <benjaminfloveday@hotmail.co.uk>
2025-04-23 15:06:07 +0100

    added toggle icon pngs and implemented the theme toggle change

--- frontend/src/components/ThemeToggle.jsx
+++ frontend/src/components/ThemeToggle.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import sunPath from "../assets/sun-red-gold-lrg.png";
+import moonPath from "../assets/moon-red-gold-lrg.png";
 
 const ThemeToggle = () => {
   const [theme, setTheme] = useState(localStorage.theme || "dark");
@@ -20,7 +22,7 @@ const ThemeToggle = () => {
       aria-label="Toggle Theme"
       className="bg-gray-100 dark:bg-gray-700 rounded px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-600 cursor-pointer"
     >
-      {theme === "dark" ? "🌞" : "🌙"}
+      {theme === "dark" ? <img src={sunPath}></img> : <img src={moonPath}></img>}
     </button>
   );
 };
