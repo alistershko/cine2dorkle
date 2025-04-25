560f6aae651321619215c3e69d1113611d336930
Fliss <fliss_d@hotmail.co.uk>
2025-04-25 12:11:17 +0100

    tests for app and main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,6 @@ import "./App.css";
 import GamePage from "./pages/game/Game";
 import { HomePage } from "./pages/home/HomePage";
 
-// const NotFoundPage = () => <div data-testid="not-found-page">404 Not Found</div>;
-
 const router = createBrowserRouter([
   {
     path: "/",
@@ -17,10 +15,6 @@ const router = createBrowserRouter([
     path: "/game",
     element: <GamePage />,
   },
-  // {
-  //   path: "*", // Catch-all route for invalid paths
-  //   element: <NotFoundPage />,
-  // },
 ]);
 
 const App = () => {
